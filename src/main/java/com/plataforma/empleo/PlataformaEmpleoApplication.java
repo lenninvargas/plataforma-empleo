023a36ca76bbcec87883a6340533276562150141
lenninvargas <lennin2666@gmail.com>
2024-10-03 02:09:16 -0500

    First commit

---
+++ src/main/java/com/plataforma/empleo/PlataformaEmpleoApplication.java
@@ -0,0 +1,13 @@
+package com.plataforma.empleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaEmpleoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaEmpleoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plataforma/empleo/PlataformaEmpleoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataforma.empleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaEmpleoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
